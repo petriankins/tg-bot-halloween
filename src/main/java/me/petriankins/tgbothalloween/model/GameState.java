50c0666cc9d33e31bf4eb4dfe1820dacc0365be3
petrise1 <petrise1@novartis.net>
2025-10-24 13:29:25 +0300

    feat: add userId and username fields to GameState class

--- src/main/java/me/petriankins/tgbothalloween/model/GameState.java
+++ src/main/java/me/petriankins/tgbothalloween/model/GameState.java
@@ -4,10 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class GameState {
+    public Long userId;
+    public String username;
+
     public int resource1;
     public int resource2;
     public long currentScenarioId;
     public Scenario currentScenario;
-    public String username;
     public Set<String> inventory = new HashSet<>();
 }
